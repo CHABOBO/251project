41b7a49583f702baa7b163239755a62e9ec8daa4
Elizabeth Chabot <elizchabot@gmail.com>
2017-12-19 17:48:42 -0600

    EC

--- Code/Gradient_Boost_Tree.py
+++ Code/Gradient_Boost_Tree.py
@@ -2,6 +2,9 @@ from pyspark.ml import Pipeline
 from pyspark.ml.classification import GBTClassifier
 from pyspark.ml.feature import StringIndexer, VectorIndexer
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+#RUN with 2 mil non-septic and 24000
+#RUN with 1 mil non-septic and 24000
+###WILL NEED TO RERUN ONCE FEATURES HAVE BEEN TUNED TO PATIENT LEVEL###
 
 gbt = GBTClassifier(labelCol="label", featuresCol="features", maxIter=10)
 
@@ -16,17 +19,25 @@ evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="p
 accuracy = evaluator.evaluate(predictions)
 print(accuracy)
 #0.989959732858
-
+#0.976641172621
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="f1")
 
 f1 = evaluator.evaluate(predictions)
 print(f1)
 
 #0.985172800629
-
+#0.965841974335
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="weightedPrecision")
 
 wp = evaluator.evaluate(predictions)
 print(wp)
 
 #0.985974308382
+#0.969283108931
+
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="weightedRecall")
+
+rc = evaluator.evaluate(predictions)
+print(rc)
+#0.98999361005
+#0.976641172621
