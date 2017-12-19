90bb04ea70bf9243fef52bbe842b81606f1c6aaa
Elizabeth Chabot <elizchabot@gmail.com>
2017-12-19 17:53:05 -0600

    ec

--- Code/Naive_Bayes.py
+++ Code/Naive_Bayes.py
@@ -6,6 +6,9 @@ from pyspark.ml.feature import VectorAssembler
 from pyspark.ml.classification import NaiveBayes
 from pyspark.ml import Pipeline
 from pyspark.ml.evaluation import MulticlassClassificationEvaluator
+#RUN WITH 2 MIL NON-SEPTIC AND 24000 SEPTIC
+#RUN WITH 1 MIL NON-SEPTIC AND 24000 SEPTIC
+###WILL NEED TO RUN AGAIN WITH FEATURES DOWN TO PATIENT###
 
 sc = SparkContext()
 sqlContext = SQLContext(sc)
@@ -29,11 +32,19 @@ predictions.first()
 accuracy = evaluator.evaluate(predictions)
 print "Model Weighted Precision: ", weightedPrecision
 #Model Weighted Precision:  0.988199872338
+                          #0.973414133329
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="accuracy")
 accuracy = evaluator.evaluate(predictions)
 print "Model Accuracy: ", accuracy
 #Model Accuracy:  0.831045640258
+                #0.829779027011
 evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="f1")
 fscore = evaluator.evaluate(predictions)
 print "Model F1-Score: ", fscore
 #Model F1-Score: 0.898684148073
+                #0.88795436768
+evaluator = MulticlassClassificationEvaluator(labelCol="label", predictionCol="prediction", metricName="weightedRecall")
+rc = evaluator.evaluate(predictions)
+print(rc)
+#0.831231900806
+#0.829779027011
